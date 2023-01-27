16d7400cd49e505bbc7e34305f04331549697454
anthonynguyen-dev <nguyen.dev@protonmail.com>
2023-01-26 18:39:48 -0600

    added id to medication model

--- models/Medication.js
+++ models/Medication.js
@@ -5,6 +5,12 @@ class Medication extends Model {}
 
 Medication.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
