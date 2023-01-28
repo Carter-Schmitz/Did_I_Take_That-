fe24ee3f36fd30d720912654754b4fffa7aa35de
Carter_Schmitz <carterschmitz95@gmail.com>
2023-01-27 21:25:24 -0600

    test of system

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const medRoutes = require('./medRoutes');
 
+
 router.use('/users', userRoutes);
 router.use('/medication', medRoutes);
 
