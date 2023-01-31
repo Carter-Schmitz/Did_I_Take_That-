daeb756be707b9f356631381e2afd2f1f9f67d32
anthonynguyen-dev <nguyen.dev@protonmail.com>
2023-01-30 19:24:12 -0600

    started login js

---
+++ public/js/login.js
@@ -0,0 +1,6 @@
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector("").value.trim();
+  const password = document.querySelector("").value.trim();
+};
